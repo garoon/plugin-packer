04cf68d10f1756d8669d1dee855d877a62c7c460
nakajmg <nakajima.jmg@gmail.com>
2020-11-10 17:30:31 +0900

    :wrench: update rollup config
    set exports option

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,24 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
-import {terser} from 'rollup-plugin-terser';
 import packageJson from "./package.json";
 
-export default [
-  {
-    input: "src/index.ts",
-    output: [{
-      name: 'garoon-plugin-packer',
-      file: "dist/garoon-plugin-packer.js",
-      format: "cjs",
-      sourcemap: 'inline'
-    }],
-    external: [...Object.keys(packageJson.devDependencies || {})],
-    plugins: [
-      resolve(),
-      typescript(),
-      commonjs({
-        extensions: ['.ts', '.js']
-      })
-    ]
-  }
-]
+export default {
+  input: "src/index.ts",
+  output: {
+    name: 'garoon-plugin-packer',
+    file: "dist/garoon-plugin-packer.js",
+    format: "cjs",
+    sourcemap: 'inline',
+    exports: "named"
+  },
+  external: [...Object.keys(packageJson.devDependencies || {})],
+  plugins: [
+    resolve(),
+    typescript(),
+    commonjs({
+      extensions: ['.ts', '.js']
+    })
+  ]
+}
+
