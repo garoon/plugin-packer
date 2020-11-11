b9cbca06aa157583f9226332eca5808f1033f8c1
nakajmg <nakajima.jmg@gmail.com>
2020-11-11 16:07:44 +0900

    :white_check_mark: update validator test

---
+++ src/__test__/validate-https-url.ts
@@ -0,0 +1,58 @@
+import assert from "assert";
+import { validateHttpsUrl } from "../validate-https-url";
+
+describe("validate-https-url", () => {
+  describe("valid", () => {
+    [
+      "https://example.com/path/to?foo=bar&baz=piyo#hash",
+      "https://localhost:8080",
+      "https://127.0.0.1:8000",
+      "https://user:pass@example.com/",
+    ].forEach((url) => {
+      it(url, () => {
+        assert(validateHttpsUrl(url));
+      });
+    });
+  });
+
+  describe("invalid", () => {
+    [
+      "http://example.com",
+      "ftp://example.com",
+      "://example.com",
+      "//example.com",
+      "/path/to/foo",
+      "./path/to/foo",
+      "path/to/foo",
+    ].forEach((url) => {
+      it(url, () => {
+        assert(!validateHttpsUrl(url));
+      });
+    });
+  });
+
+  describe("`allowHttp`", () => {
+    describe("valid", () => {
+      ["https://example.com", "http://example.com"].forEach((url) => {
+        it(url, () => {
+          assert(validateHttpsUrl(url, true));
+        });
+      });
+    });
+
+    describe("invalid", () => {
+      [
+        "ftp://example.com",
+        "://example.com",
+        "//example.com",
+        "/path/to/foo",
+        "./path/to/foo",
+        "path/to/foo",
+      ].forEach((url) => {
+        it(url, () => {
+          assert(!validateHttpsUrl(url));
+        });
+      });
+    });
+  });
+});
--- src/__test__/validator.ts
+++ src/__test__/validator.ts
@@ -1,32 +1,57 @@
 import { validateManifest } from "../validator";
 
+const validJson = (opts = {}) => {
+  return {
+    manifest_version: 1,
+    version: "1.0.0",
+    target_applications: [],
+    impacted_applications: [],
+    icon: "/path/to/icon.png",
+    name: {
+      en: "name",
+    },
+    ...opts,
+  };
+};
+
 describe("validateManifest", () => {
-  test("valid", () => {
-    expect(
-      validateManifest({
-        manifest_version: 1,
-        version: "1.0.0",
-        target_applications: ["ALL"],
-        impacted_applications: ["ADDRESS_BOOK"],
-        icon: "path/to/icon.png",
-        name: {
-          en: "validator-test",
-        },
-      })
-    ).toEqual({
+  test("minimum valid json", () => {
+    const result = validateManifest(validJson());
+    expect(result).toEqual({
       valid: true,
       errors: null,
     });
   });
 
-  test("invalid", () => {
-    expect(
-      validateManifest({
-        manifest_version: "1",
-      })
-    ).toEqual({
-      valid: true,
-      errors: null,
+  test("missingProperty", () => {
+    const json = validJson();
+    // @ts-ignore
+    delete json.name;
+    const result = validateManifest(json);
+    expect(result).toEqual({
+      valid: false,
+      errors: [
+        {
+          dataPath: ".name",
+          keyword: "required",
+          message: "is a required property",
+          params: {
+            missingProperty: "name",
+          },
+          schemaPath: "#/required",
+        },
+      ],
     });
+  });
+
+  test("invalid", () => {
+    const json = validJson({
+      manifest_version: "1",
+    });
+    // @ts-ignore
+    delete json.name;
+    const result = validateManifest(json);
+    expect(result.valid).toBe(false);
+    expect(result.errors?.length).toBe(2);
   });
 });
