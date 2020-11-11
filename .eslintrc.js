9fa48df9d79738d55e1bfca6988570fb018fdfb1
nakajmg <nakajima.jmg@gmail.com>
2020-11-11 13:25:20 +0900

    :wrench: improve eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
   extends: "@cybozu/eslint-config/presets/node-typescript-prettier",
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  settings: {
+    node: {
+      tryExtensions: [".ts", ".d.ts", ".js", ".json", ".node"],
+    },
+  },
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,24 @@
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
+/* eslint node/no-unpublished-import: 0 */
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
 import packageJson from "./package.json";
 
 export default {
   input: "src/index.ts",
   output: {
-    name: 'garoon-plugin-packer',
+    name: "garoon-plugin-packer",
     file: "dist/garoon-plugin-packer.js",
     format: "cjs",
-    sourcemap: 'inline',
-    exports: "named"
+    sourcemap: "inline",
+    exports: "named",
   },
   external: [...Object.keys(packageJson.devDependencies || {})],
   plugins: [
     resolve(),
     typescript(),
     commonjs({
-      extensions: ['.ts', '.js']
-    })
-  ]
-}
-
+      extensions: [".ts", ".js"],
+    }),
+  ],
+};
