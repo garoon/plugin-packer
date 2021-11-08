e57e857963ef3d817dc4962087f61dd39d5238c6
h000984 <hai-pham@cybozu.vn>
2021-11-08 16:32:20 +0700

    GTM-10779: remove debug code

--- test/validateManifest.ts
+++ test/validateManifest.ts
@@ -47,7 +47,6 @@ describe("validateManifest", () => {
   test("plugin-invalid-https-url", () => {
     const json = require("./fixtures/plugin-invalid-https-url/manifest.json");
     const result = validateManifest(json);
-    console.log(result.errors);
     assert(result.valid === false);
     assert(result.errors?.length === 4);
   });
